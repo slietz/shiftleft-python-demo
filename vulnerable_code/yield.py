a09206d16b3469a682ef9578ca76fc229bfc3cb8
slietz <slietz@users.noreply.github.com>
2021-01-28 06:50:24 -0800

    Update yield.py

--- vulnerable_code/yield.py
+++ vulnerable_code/yield.py
@@ -3,6 +3,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
+response.set_cookie('username', 'flask', secure=True, httponly=True, samesite='Lax')
 
 def things_to_run():
     yield "echo hello"
